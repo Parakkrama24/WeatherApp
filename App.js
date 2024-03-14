ec11ab03f467a23dcfe1c18b6fd7eeaf2be0321f
Parakkrama Dasanayaka <110714055+Parakkrama24@users.noreply.github.com>
2024-03-14 19:13:09 +0530

    test

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {Text, View, Image} from 'react-native';
 
-export default function App() {
+const CatApp = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      
+      <Text>Hello, I am your cat!</Text>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default CatApp;
